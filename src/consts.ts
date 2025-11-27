eca49160e3b549fafd2eb7f0be000255743cc4e0
deividuska <deividas.krupstas@gmail.com>
2025-11-27 12:17:48 +0000

    home page meta updated

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Eks Emigrantai';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Patarimai grįžtantiems į Lietuvą. Informacija apie bedarbio pašalpas, dokumentus, socialinį draudimą ir praktinius žingsnius emigrantams.';
